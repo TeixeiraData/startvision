5f9f0e9a5ec5004e18ebf0c5d8f973cccf67b4e4
TeixeiraData <lucas_machida@outlook.com>
2023-09-24 09:40:22 -0300

    starting

---
+++ flaskblog.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "<h1>Home page</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About page</h1>"
