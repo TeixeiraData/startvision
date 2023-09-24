74d7aa9d3686955711bff667da2ac6cc0f0328bc
TeixeiraData <lucas_machida@outlook.com>
2023-09-24 10:39:46 -0300

    templates1

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template('about.html')
