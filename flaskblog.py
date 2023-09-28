b19dbbf944076b4240f89b4b19c4b399e1252ae7
TeixeiraData <lucas_machida@outlook.com>
2023-09-28 11:59:36 -0300

    sexto dia dormindo

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '983da12b8305ed18fee8eeaf7f01eef4'
