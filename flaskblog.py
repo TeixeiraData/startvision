6d37bc9906b328298327cdade7cf6984494efae0
TeixeiraData <lucas_machida@outlook.com>
2023-10-16 10:31:14 -0300

    second

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-from models import User, Post
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '983da12b8305ed18fee8eeaf7f01eef4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+from models import User, Post
 
 posts = [
     {
